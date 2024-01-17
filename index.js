4e8e24df5dd40445c0253b4071d73f5f67b74967
anastasiiagold <Anastasiia.Goldshtein@bynet.co.il>
2024-01-17 10:11:58 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const os = require('os')
+const fs = require('fs')
+const headFile = fs.readFileSync('./.git/HEAD', 'utf-8')
+const headRef = headFile.split(': ')[1]?.trim()
+const PORT = process.env.PORT ?? 80
+if (headRef)
+    head = fs.readFileSync('./.git/' + headRef, 'utf-8')
+else head = headFile
+http.createServer((req, res) => {
+    res.end(os.hostname + ' ' + head)
+}).listen(PORT, ()=> {
+    console.log('Listening on port', PORT)
+})
